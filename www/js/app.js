7d1f389f71d43de36347998dc6605a4e728a0939
CNBorn <CNBorn@hotmail.com>
2015-04-05 06:44:57 +0800

    iteration2

--- www/js/app.js
+++ www/js/app.js
@@ -6,24 +6,48 @@ var Header = React.createClass({
     }
 });
 
-var ActionList = React.createClass({
+
+
+
+var ActionListItem = React.createClass({
     render: function () {
         return (
+                <li>
+                <a href={"#action/" + this.props.action.id}>
+                {this.props.action.kindName} {this.props.action.desc}
+            </a>
+                </li>
+        );
+    }
+});
+
+var ActionList = React.createClass({
+    render: function () {
+        var actions = this.props.actionList.map(function (action) {
+            return (
+                    <ActionListItem key={action.id} action={action} />
+            );
+        });
+        return (
                 <ul>
-                <li>Eat: 90ml 10:30pm - 10:50pm</li>
-                <li>Diaper: 10:55pm </li>
-                <li>Sleep: 11:00pm - 12:00pm</li>
-                </ul>
+                {actions}
+            </ul>
         );
     }
 });
 
 var HomePage = React.createClass({
     render: function () {
+        var actions = [
+            {kindName: 'Eat', desc: '90ml 10:30pm - 10:50pm'},
+            {kindName: 'Diaper', desc: '10:55pm'},
+            {kindName: 'Sleep', desc: '11:00pm - 12:00pm'},
+            {kindName: 'Poop', desc: '10:55pm'}
+        ];
         return (
                 <div>
                 <Header text="BabyRoutine"/>
-                <ActionList />
+                <ActionList actionList={actions}/>
                 </div>
         );
     }

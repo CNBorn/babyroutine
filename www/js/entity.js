7c7852b9c0e046948160e379c371ad17a47e3ed7
CNBorn <CNBorn@hotmail.com>
2015-04-23 06:59:33 +0800

    can render item with async

--- www/js/entity.js
+++ www/js/entity.js
@@ -8,6 +8,12 @@ var ActionPage = React.createClass({
         }.bind(this));
     },
     render: function () {
+        var propsVolume, propsDuration = '';
+        if (this.state.action.props) {
+            propsVolume = this.state.action.props.volume;
+            propsDuration = this.state.action.props.duration + 'min';
+        }
+
         return (
                 <div>
                 <Header text={this.state.action.kindName} />
@@ -16,8 +22,10 @@ var ActionPage = React.createClass({
                 <ul className="table-view">
                 <li className="table-view-cell media">
                 <img className="media-object big pull-left" src={"img/" + this.state.action.kindName + ".png"} />
-                <h1>{this.state.action.desc}</h1>
-                <h1>{this.state.action.props}</h1>
+                <h1>{moment(this.state.action.createdAt).format('HH:mm')} &nbsp;
+            {propsVolume} &nbsp; {propsDuration}
+            </h1>
+
                 </li>
                 </ul>
                 </div>

b6103e6ea0b8992bad9c7a765f3a634d4adc5310
CNBorn <CNBorn@hotmail.com>
2015-05-16 19:15:02 +0800

    make sure entity can run

--- www/js/entity.js
+++ www/js/entity.js
@@ -21,7 +21,7 @@ var ActionPage = React.createClass({
                 <div className="card">
                 <ul className="table-view">
                 <li className="table-view-cell media">
-                <img className="media-object big pull-left" src={"img/" + this.state.action.kindName.toLowerCase() + ".png"} />
+                <img className="media-object big pull-left" src={"img/" + this.state.action.kindName + ".png"} />
                 <h1>{moment(this.state.action.createdAt).format('HH:mm')} &nbsp;
             {propsVolume} &nbsp; {propsDuration}
             </h1>
